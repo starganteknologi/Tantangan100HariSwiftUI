c83a300b6f7fab08a275e4ec332bd771ba3d9e43
Fathur Rohman <vatoer.ckplus@gmail.com>
2022-03-22 22:57:09 +0200

    perubahan data type

--- hari ke-1/MyPlaygroundHari1.playground/Contents.swift
+++ hari ke-1/MyPlaygroundHari1.playground/Contents.swift
@@ -1,6 +1,24 @@
-var x = 1
+var x = 1.0
 print(x)
-x = 2
+x = 2.0 + 1
 print(x)
 
-let y = 2
+/// more coding
+///
+let y = 2.0
+
+let n = x + y
+
+let z:Int
+
+var t = "nilai y adalah ? "
+print(t + " \(y)")
+
+var multilinetext = """
+ini adalah text yang sangat panjang sekali ??
+asdasdasd
+apaspdsaspdaspd
+"""
+
+print(multilinetext)
+
